f046b7a3f8fee5305406b0f7bae82a242b1ad37a
Mohammad Attaullah <awsumfriend7396@gmail.com>
2016-09-15 10:26:54 +0500

    Update app.js
    added states to app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,13 @@
 angular.module("demoApp", [
     'ui.router',
-    'app.home'
+    'app.home',
+    'app.allList',
+    'äpp.semi',
+    'app.diodes',
+    'app.connectors',
+    'app.viewsemi',
+    'app.viewdiode',
+    'app.viewconnector'
 ])
     .controller('mainController', [mainController]);
 
